e5f9223962e76cc06e1378f822a6ca32bdb095f8
Ojo Tokunbo <ojo_tokunbo2002@yahoo.com>
2020-01-26 14:13:22 +0100

    app config update

--- config/app.config.js
+++ config/app.config.js
@@ -1,65 +1,65 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const env = process.env.NODE_ENV; // 'dev' or 'test' or prod
+// const env = process.env.NODE_ENV; // 'dev' or 'test' or prod
 
-const dev = {
+// const dev = {
     
-    app: {
-        port: parseInt(process.env.PORT) || 4000
-    },
+//     app: {
+//         port: parseInt(process.env.PORT) || 4000
+//     },
 
-    db: {
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT),
-        name: process.env.DB_NAME
-    },
+//     db: {
+//         host: process.env.DB_HOST,
+//         port: parseInt(process.env.DB_PORT),
+//         name: process.env.DB_NAME
+//     },
 
-    facebook: {
-        app_id: process.env.FACEBOOK_APP_ID,
-        app_secret: process.env.FACEBOOK_APP_SECRET,
-        callback: process.env.FACEBOOK_CALLBACK_URL
-    },
+//     facebook: {
+//         app_id: process.env.FACEBOOK_APP_ID,
+//         app_secret: process.env.FACEBOOK_APP_SECRET,
+//         callback: process.env.FACEBOOK_CALLBACK_URL
+//     },
 
-    twitter: {
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        callback: process.env.TWITTER_CALLBACK_URL
-    }
+//     twitter: {
+//         consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//         callback: process.env.TWITTER_CALLBACK_URL
+//     }
     
-};
+// };
 
 
-const test = {
+// const test = {
 
-    app: {
-        port: parseInt(process.env.TEST_APP_PORT) || 3000
-    },
+//     app: {
+//         port: parseInt(process.env.TEST_APP_PORT) || 3000
+//     },
 
-    db: {
-        host: process.env.TEST_DB_HOST || 'localhost',
-        port: parseInt(process.env.TEST_DB_PORT) || 27017,
-        name: process.env.TEST_DB_NAME || 'test'
-    },
+//     db: {
+//         host: process.env.TEST_DB_HOST || 'localhost',
+//         port: parseInt(process.env.TEST_DB_PORT) || 27017,
+//         name: process.env.TEST_DB_NAME || 'test'
+//     },
 
-    social: {
+//     social: {
 
-        facebook: {
-          app_id: process.env.FACEBOOK_APP_ID,
-          app_secret: process.env.FACEBOOK_APP_SECRET,
-          callback: process.env.FACEBOOK_CALLBACK_URL
-        },
+//         facebook: {
+//           app_id: process.env.FACEBOOK_APP_ID,
+//           app_secret: process.env.FACEBOOK_APP_SECRET,
+//           callback: process.env.FACEBOOK_CALLBACK_URL
+//         },
     
-        twitter: {
-            consumer_key: process.env.TWITTER_CONSUMER_KEY,
-            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-            callback: process.env.TWITTER_CALLBACK_URL
-        }
-      }
-};
+//         twitter: {
+//             consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//             consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//             callback: process.env.TWITTER_CALLBACK_URL
+//         }
+//       }
+// };
 
-const config = {
-    dev,
-    test
-};
+// const config = {
+//     dev,
+//     test
+// };
 
-module.exports = config[env];
+// module.exports = config[env];
